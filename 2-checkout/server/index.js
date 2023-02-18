a22d4c0e181d85bea6178d81abd623511c5fac30
Anthony Bui <anthonyb50@gmail.com>
2023-02-18 15:46:00 -0500

    Finished shipping info functionality

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.json());
 
 app.post('/signup', (req, res) => {
-  db.query(`SELECT email, submitted FROM responses WHERE email = '${req.body.email}'`, (err, result) => {
+  db.query(`SELECT email FROM responses WHERE email = '${req.body.email}'`, (err, result) => {
     if (!result.length) {
       db.query(`INSERT INTO responses (name, password, email, sessionID) VALUES ('${req.body.name}', '${req.body.password}', '${req.body.email}', '${req.session_id}')`)
       res.redirect('/shipping');
@@ -35,7 +35,6 @@ app.post('/signup', (req, res) => {
 app.post('/checkout', (req, res) => {
   db.query(`SELECT submitted FROM responses WHERE sessionID = '${req.session_id}'`, (err, result) => {
     if (!result.length || !result[0].submitted) {
-      console.log(result);
       res.redirect('/signup');
     } else {
       res.sendStatus(500);
@@ -43,12 +42,16 @@ app.post('/checkout', (req, res) => {
   })
 })
 
-
-
-
 app.post('/shipping', (req, res) => {
-  console.log(req.body);
-  res.redirect('/payment');
+  db.query(`SELECT address1, submitted FROM responses WHERE sessionID = '${req.session_id}'`, (err, result) => {
+    if (!result[0].address1) {
+      db.query(`UPDATE responses SET address1='${req.body.address1}', address2='${req.body.address2}', city='${req.body.city}',
+      state='${req.body.state}', shipZip='${req.body.shipZip}', phoneNumber='${req.body.phoneNumber}' WHERE sessionID='${req.session_id}'`)
+      res.redirect('/payment');
+    } else {
+      res.sendStatus(500);
+    }
+  })
 })
 
 app.post('/payment', (req, res) => {
